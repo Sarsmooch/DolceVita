1899db4188d68cc4e1bf607f918d644591c089e7
Luiza Ramos <85586134+Sarsmooch@users.noreply.github.com>
2023-06-06 14:30:31 -0300

    layout update

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -91,7 +91,7 @@
                 </li>
             @endforeach
         </ul>
-        <p class="text-center">© {{ date('Y') }} {{ env('APP_NAME') }}. Todos os direitos reservados.</p>
+        <p class="copyright">© {{ date('Y') }} {{ env('APP_NAME') }}. Todos os direitos reservados.</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
